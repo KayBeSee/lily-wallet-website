39eb522c995c297155945ec73cabe2c55f381891
Kevin Mulcrone <kaybesee@gmail.com>
2020-08-09 00:19:38 -0600

    not doing state right

--- js/Nav.js
+++ js/Nav.js
@@ -23,7 +23,7 @@ class Nav extends React.Component {
                     Wallet</span>
                 </a>
                 <div className="-mr-2 flex items-center md:hidden">
-                  <button type="button" onClick={this.setState({ isOpen: !isOpen })}
+                  <button type="button" onClick={this.setState({ isOpen: !this.state.isOpen })}
                     className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                     id="main-menu" aria-label="Main menu" aria-haspopup="true">
                     <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -59,7 +59,7 @@ class Nav extends React.Component {
 
 
         <Transition
-          show={isOpen}
+          show={this.state.isOpen}
           enter="duration-150 ease-out"
           enterFrom="opacity-0 scale-95"
           enterTo="opacity-100 scale-100"
