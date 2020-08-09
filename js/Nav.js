be16079d6bad6dc89d362e6e2fe303a6a13f5990
Kevin Mulcrone <kaybesee@gmail.com>
2020-08-09 01:08:18 -0600

    again

--- js/Nav.js
+++ js/Nav.js
@@ -180,10 +180,10 @@ class Nav extends React.Component {
                 aria-labelledby="main-menu">
                 <div className="px-5 pt-4 flex items-center justify-between">
                   <div>
-                    <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-on-white.svg" alt="" />
+                    <img className="h-8 w-auto" src="Logo.svg" alt="" />
                   </div>
                   <div className="-mr-2">
-                    <button type="button"
+                    <button type="button" onClick={() => { this.setState({ isOpen: !this.state.isOpen }) }}
                       className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                       aria-label="Close menu">
                       <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
