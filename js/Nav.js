619d37db33946a952d8abe3bc515625f7e8cf32e
Kevin Mulcrone <kaybesee@gmail.com>
2020-08-09 00:23:02 -0600

    clearing more errors

--- js/Nav.js
+++ js/Nav.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const e = React.createElement;
-const Fragment = React.Fragment
+const Fragment = React.Fragment;
+const Transition = './Transition';
 
 class Nav extends React.Component {
   constructor(props) {
@@ -23,7 +24,7 @@ class Nav extends React.Component {
                     Wallet</span>
                 </a>
                 <div className="-mr-2 flex items-center md:hidden">
-                  <button type="button" onClick={this.setState({ isOpen: !this.state.isOpen })}
+                  <button type="button" onClick={() => { this.setState({ isOpen: !this.state.isOpen }) }}
                     className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                     id="main-menu" aria-label="Main menu" aria-haspopup="true">
                     <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
