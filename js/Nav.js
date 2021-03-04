177487324870be66fc67f9c9f8b466f851884a51
Kevin Mulcrone <kaybesee@gmail.com>
2021-03-03 20:42:43 -0700

    update nav

--- js/Nav.js
+++ js/Nav.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const Fragment = React.Fragment;
 // import Transition from './Transition.js';
@@ -10,40 +10,40 @@ const { CSSTransition } = ReactTransitionGroup;
 
 const TransitionContext = React.createContext({
   parent: {},
-})
+});
 
 function useIsInitialRender() {
-  const isInitialRender = useRef(true)
+  const isInitialRender = useRef(true);
   useEffect(() => {
-    isInitialRender.current = false
-  }, [])
-  return isInitialRender.current
+    isInitialRender.current = false;
+  }, []);
+  return isInitialRender.current;
 }
 
 function MyTransition({
   show,
-  enter = '',
-  enterFrom = '',
-  enterTo = '',
-  leave = '',
-  leaveFrom = '',
-  leaveTo = '',
+  enter = "",
+  enterFrom = "",
+  enterTo = "",
+  leave = "",
+  leaveFrom = "",
+  leaveTo = "",
   appear,
   children,
 }) {
-  const enterClasses = enter.split(' ').filter((s) => s.length)
-  const enterFromClasses = enterFrom.split(' ').filter((s) => s.length)
-  const enterToClasses = enterTo.split(' ').filter((s) => s.length)
-  const leaveClasses = leave.split(' ').filter((s) => s.length)
-  const leaveFromClasses = leaveFrom.split(' ').filter((s) => s.length)
-  const leaveToClasses = leaveTo.split(' ').filter((s) => s.length)
+  const enterClasses = enter.split(" ").filter((s) => s.length);
+  const enterFromClasses = enterFrom.split(" ").filter((s) => s.length);
+  const enterToClasses = enterTo.split(" ").filter((s) => s.length);
+  const leaveClasses = leave.split(" ").filter((s) => s.length);
+  const leaveFromClasses = leaveFrom.split(" ").filter((s) => s.length);
+  const leaveToClasses = leaveTo.split(" ").filter((s) => s.length);
 
   function addClasses(node, classes) {
-    classes.length && node.classList.add(...classes)
+    classes.length && node.classList.add(...classes);
   }
 
   function removeClasses(node, classes) {
-    classes.length && node.classList.remove(...classes)
+    classes.length && node.classList.remove(...classes);
   }
 
   return (
@@ -52,38 +52,38 @@ function MyTransition({
       unmountOnExit
       in={show}
       addEndListener={(node, done) => {
-        node.addEventListener('transitionend', done, false)
+        node.addEventListener("transitionend", done, false);
       }}
       onEnter={(node) => {
-        addClasses(node, [...enterClasses, ...enterFromClasses])
+        addClasses(node, [...enterClasses, ...enterFromClasses]);
       }}
       onEntering={(node) => {
-        removeClasses(node, enterFromClasses)
-        addClasses(node, enterToClasses)
+        removeClasses(node, enterFromClasses);
+        addClasses(node, enterToClasses);
       }}
       onEntered={(node) => {
-        removeClasses(node, [...enterToClasses, ...enterClasses])
+        removeClasses(node, [...enterToClasses, ...enterClasses]);
       }}
       onExit={(node) => {
-        addClasses(node, [...leaveClasses, ...leaveFromClasses])
+        addClasses(node, [...leaveClasses, ...leaveFromClasses]);
       }}
       onExiting={(node) => {
-        removeClasses(node, leaveFromClasses)
-        addClasses(node, leaveToClasses)
+        removeClasses(node, leaveFromClasses);
+        addClasses(node, leaveToClasses);
       }}
       onExited={(node) => {
-        removeClasses(node, [...leaveToClasses, ...leaveClasses])
+        removeClasses(node, [...leaveToClasses, ...leaveClasses]);
       }}
     >
       {children}
     </CSSTransition>
-  )
+  );
 }
 
 const Transition = ({ show, appear, ...rest }) => {
-  const { parent } = useContext(TransitionContext)
-  const isInitialRender = useIsInitialRender()
-  const isChild = show === undefined
+  const { parent } = useContext(TransitionContext);
+  const isInitialRender = useIsInitialRender();
+  const isChild = show === undefined;
 
   if (isChild) {
     return (
@@ -92,7 +92,7 @@ const Transition = ({ show, appear, ...rest }) => {
         show={parent.show}
         {...rest}
       />
-    )
+    );
   }
 
   return (
@@ -107,8 +107,8 @@ const Transition = ({ show, appear, ...rest }) => {
     >
       <MyTransition appear={appear} show={show} {...rest} />
     </TransitionContext.Provider>
-  )
-}
+  );
+};
 
 class Nav extends React.Component {
   constructor(props) {
@@ -117,51 +117,98 @@ class Nav extends React.Component {
   }
 
   render() {
-
     return (
       <Fragment>
         <div className="max-w-screen-xl mx-auto px-4 sm:px-6">
           <nav className="relative flex items-center justify-between sm:h-10 md:justify-center">
             <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
               <div className="flex items-center justify-between w-full md:w-auto">
-                <a href="https://lily.kevinmulcrone.com/" aria-label="Home" style={{ display: 'flex', alignItems: 'center' }}>
-                  <img className="h-8 w-auto sm:h-10" style={{ margin: '0 0 .5em' }} src="logo.svg" alt="Logo" />
-                  <span className="font-medium" style={{ paddingLeft: '.5em', fontSize: '1.25em' }}>Lily
-                    Wallet</span>
+                <a
+                  href="https://lily.kevinmulcrone.com/"
+                  aria-label="Home"
+                  style={{ display: "flex", alignItems: "center" }}
+                >
+                  <img
+                    className="h-8 w-auto sm:h-10"
+                    style={{ margin: "0 0 .5em" }}
+                    src="logo.svg"
+                    alt="Logo"
+                  />
+                  <span
+                    className="font-medium"
+                    style={{ paddingLeft: ".5em", fontSize: "1.25em" }}
+                  >
+                    Lily Wallet
+                  </span>
                 </a>
                 <div className="-mr-2 flex items-center md:hidden">
-                  <button type="button" onClick={() => { this.setState({ isOpen: !this.state.isOpen }) }}
+                  <button
+                    type="button"
+                    onClick={() => {
+                      this.setState({ isOpen: !this.state.isOpen });
+                    }}
                     className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                    id="main-menu" aria-label="Main menu" aria-haspopup="true">
-                    <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                        d="M4 6h16M4 12h16M4 18h16" />
+                    id="main-menu"
+                    aria-label="Main menu"
+                    aria-haspopup="true"
+                  >
+                    <svg
+                      className="h-6 w-6"
+                      stroke="currentColor"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
                     </svg>
                   </button>
                 </div>
               </div>
             </div>
             <div className="hidden md:flex md:space-x-10">
-              <a href="https://lily.kevinmulcrone.com#features"
-                className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Features</a>
-              <a href="https://lily.kevinmulcrone.com#security"
-                className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Security</a>
-              <a href="https://docs.lily.kevinmulcrone.com"
-                className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Docs</a>
-              <a href="mailto:kaybesee@gmail.com"
-                className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Support</a>
+              <a
+                href="https://lily.kevinmulcrone.com#features"
+                className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
+              >
+                Features
+              </a>
+              <a
+                href="https://lily.kevinmulcrone.com#security"
+                className="font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out"
+              >
+                Security
+              </a>
+              <a
+                href="https://lily.kevinmulcrone.com/pricing.html"
+                className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
+                role="menuitem"
+              >
+                Pricing
+              </a>
+              <a
+                href="https://lily.kevinmulcrone.com/pricing.html"
+                className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
+                role="menuitem"
+              >
+                Support
+              </a>
             </div>
             <div className="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
               <span className="inline-flex rounded-md shadow">
-                <a href="https://lily.kevinmulcrone.com#download"
-                  className="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-yellow-300 bg-white hover:text-yellow-400 focus:outline-none focus:border-yellow-300 focus:shadow-outline-yellow active:bg-gray-50 active:text-yellow-700 transition duration-150 ease-in-out">
+                <a
+                  href="https://lily.kevinmulcrone.com#download"
+                  className="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-yellow-300 bg-white hover:text-yellow-400 focus:outline-none focus:border-yellow-300 focus:shadow-outline-yellow active:bg-gray-50 active:text-yellow-700 transition duration-150 ease-in-out"
+                >
                   Download
                 </a>
               </span>
             </div>
           </nav>
         </div>
-
 
         <Transition
           show={this.state.isOpen}
@@ -172,10 +219,17 @@ class Nav extends React.Component {
           leaveFrom="opacity-100 scale-100"
           leaveTo="opacity-0 scale-95"
         >
-          <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden" style={{ zIndex: 1 }}>
+          <div
+            className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+            style={{ zIndex: 1 }}
+          >
             <div className="rounded-lg shadow-md">
-              <div className="rounded-lg bg-white shadow-xs overflow-hidden" role="menu" aria-orientation="vertical"
-                aria-labelledby="main-menu">
+              <div
+                className="rounded-lg bg-white shadow-xs overflow-hidden"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="main-menu"
+              >
                 <div className="px-5 pt-4 flex items-center justify-between">
                   <a href="https://lily.kevinmulcrone.com/">
                     <div>
@@ -183,45 +237,78 @@ class Nav extends React.Component {
                     </div>
                   </a>
                   <div className="-mr-2">
-                    <button type="button" onClick={() => { this.setState({ isOpen: !this.state.isOpen }) }}
+                    <button
+                      type="button"
+                      onClick={() => {
+                        this.setState({ isOpen: !this.state.isOpen });
+                      }}
                       className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-                      aria-label="Close menu">
-                      <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+                      aria-label="Close menu"
+                    >
+                      <svg
+                        className="h-6 w-6"
+                        stroke="currentColor"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
                       </svg>
                     </button>
                   </div>
                 </div>
                 <div className="px-2 pt-2 pb-3">
-                  <a href="https://lily.kevinmulcrone.com#features"
+                  <a
+                    href="https://lily.kevinmulcrone.com#features"
                     className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
-                    role="menuitem">Features</a>
-                  <a href="https://lily.kevinmulcrone.com#security"
+                    role="menuitem"
+                  >
+                    Features
+                  </a>
+                  <a
+                    href="https://lily.kevinmulcrone.com#security"
                     className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
-                    role="menuitem">Security</a>
-                  <a href="https://docs.lily.kevinmulcrone.com"
+                    role="menuitem"
+                  >
+                    Security
+                  </a>
+                  <a
+                    href="https://lily.kevinmulcrone.com/pricing.html"
                     className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
-                    role="menuitem">Docs</a>
-                  <a href="mailto:kaybesee@gmail.com"
+                    role="menuitem"
+                  >
+                    Pricing
+                  </a>
+                  <a
+                    href="https://docs.lily.kevinmulcrone.com"
                     className="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out"
-                    role="menuitem">Support</a>
+                    role="menuitem"
+                  >
+                    Support
+                  </a>
                 </div>
                 <div>
-                  <a href="https://lily.kevinmulcrone.com#download"
+                  <a
+                    href="https://lily.kevinmulcrone.com#download"
                     className="rounded-md block w-full px-5 py-3 text-center font-medium text-white bg-green-700 hover:bg-gray-100 hover:text-yellow-700 focus:outline-none focus:bg-gray-100 focus:text-yellow-700 transition duration-150 ease-in-out"
-                    role="menuitem">
+                    role="menuitem"
+                  >
                     Download
-                </a>
+                  </a>
                 </div>
               </div>
             </div>
           </div>
         </Transition>
       </Fragment>
-    )
+    );
   }
 }
 
-const domContainer = document.querySelector('#navnav');
-console.log('domContainer: ', domContainer);
+const domContainer = document.querySelector("#navnav");
+console.log("domContainer: ", domContainer);
 ReactDOM.render(<Nav />, domContainer);
